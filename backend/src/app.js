8c582cf226d3a35d5002a1f31abc9ac3f44fb701
Mirza Adi Raffiansyah <uniqueaddress19@gmail.com>
2026-04-30 21:31:33 +0700

    AGAIN

--- backend/src/app.js
+++ backend/src/app.js
@@ -48,7 +48,7 @@ app.use(helmet());
 
 // CORS configuration
 const corsOptions = {
-  origin: 'http://localhost:5173',
+  origin: '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 };
