5587dad929357c75e2b10e0cd1c23918f0598823
Mirza Adi Raffiansyah <uniqueaddress19@gmail.com>
2026-04-30 20:47:14 +0700

    fix: add ssl setting for cloud db

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -6,7 +6,12 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
+
 
 pool.on('connect', () => {
   console.log('Connected to PostgreSQL database');
